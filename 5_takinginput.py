e8cfe5065a3c9891c90ca2c5800b7843f06f060a
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-07 17:22:10 +0545

    User Input

---
+++ 5_takinginput.py
@@ -0,0 +1,11 @@
+#String
+name = input("Enter your name: ")
+print(name)
+
+#Integer
+age = int(input("Enter your age: "))
+print(age)
+
+salary = float(input("Enter your salary: "))
+print(salary)
+
