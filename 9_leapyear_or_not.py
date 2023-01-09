62ea2d54d5167ea3b06547993b7cda30a0a804e4
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-09 19:54:06 +0545

    Initial Commit

---
+++ 9_leapyear_or_not.py
@@ -0,0 +1,13 @@
+# Enter the year to find if it is leap year or not:
+year = int(input("Enter the year: "))
+
+if(year % 4) == 0:
+    if(year % 100 == 0):
+        if(year % 400 == 0):
+            print(f'{year} is a leap year')
+        else:
+            print(f'{year} is not a leap year')
+    else:
+         print(f'{year} is a leap year')
+else:
+     print(f'{year} is not a leap year')
