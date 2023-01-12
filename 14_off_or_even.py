f4fb81b824aafd9b16e0da63bc76c69f419c6a1f
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-12 18:47:53 +0545

    Initial Commit

---
+++ 14_off_or_even.py
@@ -0,0 +1,12 @@
+# Taking input from user through prompt
+
+num = int(input("Enter any number: "))
+flag = num % 2
+if flag == 0:
+    print(num, "is an even number")
+elif flag == 1:
+    print(num, "is an odd number")
+else:
+    print("Error, Invalid input")
+
+
