d585e8479bd73b26e1c51cb7fe0947a0a0771275
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-25 17:20:32 +0545

    creating a list #2 methods

---
+++ 26CreateOrDeclareList.py
@@ -0,0 +1,15 @@
+#1st method
+
+a = []
+
+print("value of a:",a)
+print("Type of a:",type(a))
+print("size of a:",len(a))
+
+#2nd method
+
+b = list()
+
+print("value of a:",b)
+print("Type of a:",type(b))
+print("size of a:",len(b))
