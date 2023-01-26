ba043579ffe06f213b6ea63ade7359cf95252f10
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-26 18:40:28 +0545

    Adding Binary numbers

--- 38AddingBinaryNumbers.py
+++ 38AddingBinaryNumbers.py
@@ -1,5 +1,7 @@
-num1 = '00011'
-num2 = '10001'
+num1 = '011'
+num2 = '001'
 
 sum = bin(int(num1,2) + int(num2,2))  #int(num,base of 2)
-print("Sum is: ",sum)
+print(sum)
+
+#result is 0b100 -> 100 is the value and b represents binary format
