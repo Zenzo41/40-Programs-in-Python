5021afaf049b705d53d73948b6839586ed4b81ef
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-26 18:24:29 +0545

    Swapping without variable

---
+++ 36SwappingWithoutAnotherVariable.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the value for num1: "))
+num2 = int(input("Enter the value for num2: "))
+
+print("")
+print("Value of num1 before swapping:",num1)
+print("Value of num2 before swapping:",num2)
+
+num1, num2 = num2 , num1
+
+print("")
+print("Value of num1 after swapping:",num1)
+print("Value of num2 after swapping:",num2)
